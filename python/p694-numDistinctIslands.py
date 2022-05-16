68227029aa855b39b787ce019e8aa841ad39e02c
nightcat <you@example.com>
2022-05-16 19:21:05 +0800

    bfs using deque

--- python/p694-numDistinctIslands.py
+++ python/p694-numDistinctIslands.py
@@ -4,27 +4,26 @@ class Solution:
 
     def numDistinctIslands(self, grid: list[list[int]]) -> int:
 
-        # dfs to find island, return the list of point in island
-        def bfs(grid: list[list[int]], x: int, y: int, li: list[tuple]):
-            if (x, y) not in li:
-                grid[x][y] = 2
-                li.append((x, y))
-                for dx, dy in dirs:
-                    nx, ny = x+dx, y+dy
-                    if 0 <= nx < m and 0 <= ny < n and grid[nx][ny]==1:
-                        q.append((nx, ny))
-
+        # bfs to find island, return the list of point in island
+        def bfs(grid: list[list[int]]) -> list[tuple]:
+            dirs = ((-1,0),(1,0),(0,-1),(0,1))
+            li = []
             while q:
-                nx, ny = q.popleft()
-                li = bfs(grid, nx, ny, li)
-
-            return
-
+                x, y = q.popleft()
+                if (x, y) not in li:
+                    grid[x][y] = 2
+                    li.append((x, y))
+                    for dx, dy in dirs:
+                        nx, ny = x+dx, y+dy
+                        if 0 <= nx < m and 0 <= ny < n and grid[nx][ny]==1:
+                            q.append((nx, ny))
+            return li
+            
 
         # modify the island point list relative
         def normalize(ld: list[tuple]) -> list[tuple]:
             ld.sort()
-            x0, y0 = ld[0][0], ld[0][1]
+            x0, y0 = ld[0]
             ld[0] = (0, 0)
             for i in range(1, len(ld)):
                 ld[i] = (ld[i][0]-x0, ld[i][1]-y0)
@@ -32,15 +31,14 @@ class Solution:
 
 
         m, n = len(grid), len(grid[0])
-        dirs = ((-1,0),(1,0),(0,-1),(0,1))
         unique = set()
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
                     q = deque()
-                    r = []
-                    bfs(grid, i, j, r)
+                    q.append((i,j))
+                    r = bfs(grid)
                     #print(r)
                     unique.add(normalize(r))
 
@@ -66,5 +64,5 @@ def run(*args, **kw):
     print(r)
 
 grid = [[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]]
-grid = [[1,1,0,1,1],[1,0,0,0,0],[0,0,0,0,1],[1,1,0,1,1]]
+#grid = [[1,1,0,1,1],[1,0,0,0,0],[0,0,0,0,1],[1,1,0,1,1]]
 run(grid)
