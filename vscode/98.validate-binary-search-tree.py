7dbb1fde6f0a3dfc14da4946394ce5002477c1ed
nightcat <you@example.com>
2022-05-17 22:50:31 +0800

    recursion with high and low boundary

--- vscode/98.validate-binary-search-tree.py
+++ vscode/98.validate-binary-search-tree.py
@@ -3,6 +3,9 @@
 #
 # [98] Validate Binary Search Tree
 #
+import math
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -11,43 +14,56 @@ class TreeNode:
 
 # @lc code=start
 class Solution:
-    def isV(self, root: TreeNode) -> bool:
-        
-        if not root.left and not root.right: 
-            return root.val, root.val
 
-        elif root.left and not root.right:
-            rll, rlr = self.isV(root.left)
-            if not rll: 
-                return None, None
-            if rlr < root.val:
-                return rll, root.val
+    def isValidBST2(self, root: TreeNode) -> bool:
+
+        def isV(root: TreeNode) -> bool:
+            
+            if not root.left and not root.right: 
+                return root.val, root.val
+
+            elif root.left and not root.right:
+                rll, rlr = isV(root.left)
+                if not rll: 
+                    return None, None
+                if rlr < root.val:
+                    return rll, root.val
+                else:
+                    return None, None
+
+            elif not root.left and root.right:
+                rrl, rrr = isV(root.right)
+                if not rrl:
+                    return None, None
+                if root.val < rrl:
+                    return root.val, rrr
+                else:
+                    return None, None
+
             else:
-                return None, None
+                rll, rlr = isV(root.left)
+                rrl, rrr = isV(root.right)
 
-        elif not root.left and root.right:
-            rrl, rrr = self.isV(root.right)
-            if not rrl:
-                return None, None
-            if root.val < rrl:
-                return root.val, rrr
-            else:
-                return None, None
+                if rll is None or rrl is None: 
+                    return None, None
+                if rlr < root.val < rrl:
+                    return rll, rrr
+                else:
+                    return None, None      
 
-        else:
-            rll, rlr = self.isV(root.left)
-            rrl, rrr = self.isV(root.right)
+        r, _ = isV(root)
+        return r is not None
 
-            if rll is None or rrl is None: 
-                return None, None
-            if rlr < root.val < rrl:
-                return rll, rrr
-            else:
-                return None, None
 
     def isValidBST(self, root: TreeNode) -> bool:
-        r, _ = self.isV(root)
-        return r is not None
+
+        def valid(root: TreeNode, low: int, high: int) -> bool:
+
+            if not root: return True
+            if root.val<=low or root.val>=high: return False
+            return valid(root.left, low, root.val) and valid(root.right, root.val, high)
+
+        return valid(root, -math.inf, math.inf)
         
         
 # @lc code=end
@@ -85,11 +101,11 @@ class Tree:
         return a[0]
 
 root = [5,1,4,None,None,3,6]
-#root = [2,1,3]
-root = [5,4,6,None,None,3,7]
+root = [2,1,3]
+#root = [5,4,6,None,None,3,7]
 #root = [1,1]
-root = [32,26,47,19,None,None,56,None,27]
-root = [5,1,4,None,None,3,6]
+#root = [32,26,47,19,None,None,56,None,27]
+
 
 tree = Tree.build(root)
 so = Solution()
