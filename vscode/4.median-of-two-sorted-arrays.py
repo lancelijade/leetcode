eb7a2b188ecedc70556b31631d43d4ad9da5d4c5
nightcat <you@example.com>
2022-05-27 00:05:21 +0800

    brute force directly

--- vscode/4.median-of-two-sorted-arrays.py
+++ vscode/4.median-of-two-sorted-arrays.py
@@ -9,7 +9,7 @@ import heapq
 
 
 class Solution:
-    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+    def findMedianSortedArrays2(self, nums1: list[int], nums2: list[int]) -> float:
         e1 = len(nums1)
         e2 = len(nums2)
         total = e1 + e2
@@ -23,13 +23,27 @@ class Solution:
             r = heapq.nlargest(total//2+1, h)[-1]
             return r
         
+        
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        total = len(nums1) + len(nums2)
 
+        a = sorted(nums1 + nums2)
+        if total % 2 == 0:
+            return (a[total//2-1] + a[total//2])/2
+        else:
+            return a[total//2]
+        
+        """
+        2094/2094 cases passed (94 ms)
+        Your runtime beats 91.41 % of python3 submissions
+        Your memory usage beats 68.33 % of python3 submissions (14.1 MB)
+        """        
 
         
 # @lc code=end
 
 nums1 = [1,2]
-nums2 = [3,4]
+nums2 = [3,4,5]
 
 so = Solution()
 r = so.findMedianSortedArrays(nums1, nums2)
