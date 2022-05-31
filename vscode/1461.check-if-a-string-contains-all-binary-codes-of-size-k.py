36569eea0f6c4e7d6eeede580363aa97cda5244c
nightcat <you@example.com>
2022-05-31 09:58:45 +0800

    slide window and mark list

---
+++ vscode/1461.check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=1461 lang=python3
+#
+# [1461] Check If a String Contains All Binary Codes of Size K
+#
+
+# @lc code=start
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        a = [0] * 2 ** k
+        for i in range(len(s)-k+1):
+            n = int(s[i:i+k], 2)
+            a[n] = 1
+        #print(a)
+        return sum(a) == 2 ** k
+
+        
+# @lc code=end
+
+s = "00110110"
+k = 1
+
+so = Solution()
+r = so.hasAllCodes(s, k)
+print(r)
