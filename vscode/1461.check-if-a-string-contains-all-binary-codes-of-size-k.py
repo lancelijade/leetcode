3afbf06c5ebdd79f1b5d2a64b2c07e2de35ebb05
nightcat <you@example.com>
2022-05-31 10:03:18 +0800

    set

--- vscode/1461.check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ vscode/1461.check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -6,18 +6,28 @@
 
 # @lc code=start
 class Solution:
+    def hasAllCodes2(self, s: str, k: int) -> bool:
+        a = [0] * 2 ** k
+        for i in range(len(s)-k+1):
+            n = int(s[i:i+k], 2)
+            a[n] = 1
+        #print(a)
+        return sum(a) == 2**k
+
+
     def hasAllCodes(self, s: str, k: int) -> bool:
         t = set()
         for i in range(len(s)-k+1):
             t.add(s[i:i+k])
-        print(t)
+        #print(t)
         return len(t) == 2 ** k
+
 
         
 # @lc code=end
 
-s = "00110110"
-k = 3
+s = "0110"
+k = 2
 
 so = Solution()
 r = so.hasAllCodes(s, k)
