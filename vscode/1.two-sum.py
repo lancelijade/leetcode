c92428d8ab8db7b8e8a16ba12a87ec47cebfa44a
nightcat <you@example.com>
2022-06-12 10:58:01 +0800

    brute force and hash map

--- vscode/1.two-sum.py
+++ vscode/1.two-sum.py
@@ -5,20 +5,33 @@
 #
 
 # @lc code=start
+from collections import defaultdict
+
+
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def twoSum2(self, nums: list[int], target: int) -> list[int]:
         le = len(nums)
         for i in range(le):
             for j in range(i + 1, le):
                 if nums[i] + nums[j] == target: return [i, j]
+
+
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        d = defaultdict(int)
+        for i, n in enumerate(nums):
+            if target - n in d:
+                return [d[target-n], i]
+            d[n] = i
+
         
 # @lc code=end
 
 nums = [2,7,11,15]
 target = 9
 
-nums = [3,3]
-target = 6
+#nums = [3,3]
+#target = 6
+
 
 so = Solution()
 r = so.twoSum(nums, target)
