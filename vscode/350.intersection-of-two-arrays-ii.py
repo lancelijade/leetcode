bf2bcadc08aa6f3da31b0d3edd3d2b6e2e313f3d
nightcat <you@example.com>
2022-06-15 00:32:35 +0800

    Counter * 2

--- vscode/350.intersection-of-two-arrays-ii.py
+++ vscode/350.intersection-of-two-arrays-ii.py
@@ -31,7 +31,7 @@ class Solution:
         return lcs(len(nums1), len(nums2))
 
 
-    def intersect(self, nums1: list[int], nums2: list[int]) -> list[int]:
+    def intersect3(self, nums1: list[int], nums2: list[int]) -> list[int]:
 
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
@@ -45,12 +45,25 @@ class Solution:
 
         return r
 
+    def intersect(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        c1 = Counter(nums1)
+        c2 = Counter(nums2)
+        c1 &= c2
+
+        r = []
+        for k, v in c1.items():
+            #print([k] * v)
+            r.extend([k] * v)
+
+        return r
+
         
 # @lc code=end
 
-nums1 = [1,2,2,1]
+nums1 = [1,2,2,2,1]
 nums2 = [2,2]
 
+"""
 nums1 = [4,9,5]
 nums2 = [9,4,9,8,4]
 
@@ -60,6 +73,7 @@ nums2 = [1,2]
 nums1 = [61,24,20,58,95,53,17,32,45,85,70,20,83,62,35,89,5,95,12,86,58,77,30,64,46,13,5,92,67,40,20,38,31,18,89,85,7,30,67,34,62,35,47,98,3,41,53,26,66,40,54,44,57,46,70,60,4,63,82,42,65,59,17,98,29,72,1,96,82,66,98,6,92,31,43,81,88,60,10,55,66,82,0,79,11,81]
 nums2 = [5,25,4,39,57,49,93,79,7,8,49,89,2,7,73,88,45,15,34,92,84,38,85,34,16,6,99,0,2,36,68,52,73,50,77,44,61,48]
 
+"""
 so = Solution()
 r = so.intersect(nums1, nums2)
 print(r)
