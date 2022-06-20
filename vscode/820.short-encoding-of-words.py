7322c3d282d3c86344d823be3ef946b5304bb145
nightcat <you@example.com>
2022-06-20 11:16:37 +0800

    official solution, trie

--- vscode/820.short-encoding-of-words.py
+++ vscode/820.short-encoding-of-words.py
@@ -5,30 +5,39 @@
 #
 
 # @lc code=start
-class Solution:
-    def minimumLengthEncoding(self, words: list[str]) -> int:
-        #wd = {}
-        #for i, word in enumerate(words):
-        #    wd[word] = i
+from collections import defaultdict
+from functools import reduce
 
-        indices = [-1] * len(words)
-        #print(wd, indices)
+
+class Solution:
+    def minimumLengthEncoding2(self, words: list[str]) -> int:
 
         ws = sorted(words, key=len, reverse=True)
         r = ''
         for w in ws:
             pos = r.find(w + '#')
-            if pos != -1:
-                1
-                #indices[wd[w]] = pos
-            else:
-                pos = len(r)
-                #indices[wd[w]] = pos
+            if pos == -1:
                 r += w + '#'
 
         #print(indices)
         return len(r)
 
+
+    def minimumLengthEncoding(self, words):
+        words = list(set(words)) #remove duplicates
+        #Trie is a nested dictionary with nodes created
+        # when fetched entries are missing
+        Trie = lambda: defaultdict(Trie)
+        trie = Trie()
+
+        #reduce(..., S, trie) is trie[S[0]][S[1]][S[2]][...][S[S.length - 1]]
+        nodes = [reduce(dict.__getitem__, word[::-1], trie)
+                 for word in words]
+
+        #Add word to the answer if it's node has no neighbors
+        return sum(len(word) + 1
+                   for i, word in enumerate(words)
+                   if len(nodes[i]) == 0)
 
 
         
