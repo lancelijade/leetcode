36262660cb06d0e1ad5c16f66a667716d81c8c78
nightcat <you@example.com>
2022-07-01 14:24:43 +0800

    sort and iterate

---
+++ vscode/1710.maximum-units-on-a-truck.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=1710 lang=python3
+#
+# [1710] Maximum Units on a Truck
+#
+
+# @lc code=start
+class Solution:
+    def maximumUnits(self, boxTypes: list[list[int]], truckSize: int) -> int:
+        bt = sorted(boxTypes, key=lambda x:x[1], reverse=True)
+        #print(bt)
+
+        rn = 0
+        r = 0
+        for n, cnt in bt:
+            if n + rn <= truckSize:
+                rn += n
+                r += n * cnt
+            else:
+                r += (truckSize - rn) * cnt
+                rn = truckSize
+            #print(rn, r, n, cnt)
+
+        return r
+
+        
+# @lc code=end
+
+boxTypes = [[5,10],[2,5],[4,7],[3,9]]
+truckSize = 10
+
+boxTypes = [[1,3],[2,2],[3,1]]
+truckSize = 4
+
+so = Solution()
+r = so.maximumUnits(boxTypes, truckSize)
+print(r)
